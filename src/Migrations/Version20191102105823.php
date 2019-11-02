b65c79c065123c58202239abd2d521129fa37cc2
Rodrigo Cinto <rodcinto@gmail.com>
2019-11-02 12:22:14 +0100

    Migrate Simulation.

---
+++ src/Migrations/Version20191102105823.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191102105823 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE simulation (id INT AUTO_INCREMENT NOT NULL, category VARCHAR(100) NOT NULL, token VARCHAR(255) DEFAULT NULL, http_verb VARCHAR(10) NOT NULL, parameters LONGTEXT DEFAULT NULL, request_body_content LONGTEXT DEFAULT NULL, response_body_content LONGTEXT DEFAULT NULL, response_content_type VARCHAR(100) DEFAULT NULL, response_delay INT DEFAULT NULL, ttl INT DEFAULT NULL, query_string VARCHAR(255) DEFAULT NULL, active TINYINT(1) NOT NULL, created DATETIME NOT NULL, updated DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE simulation');
+    }
+}
