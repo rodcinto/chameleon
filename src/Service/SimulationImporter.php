224ed4f20e897981847e10c303b72c33b38d3f1a
Rodrigo Cinto <rodcinto@gmail.com>
2020-01-12 21:35:00 +0100

    Importer Service.

---
+++ src/Service/SimulationImporter.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Service;
+
+use DateTime;
+use App\Entity\Simulation;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+class SimulationImporter
+{
+    const SERIALIZE_FORMAT = 'json';
+
+    private $serializedSimulation;
+
+    public function __construct(string $serializedSimulation)
+    {
+        $this->serializedSimulation = $serializedSimulation;
+    }
+
+    public function loadSimulation()
+    {
+        $simulation = $this->createRawSimulation();
+
+        $normalizer = [new ObjectNormalizer()];
+        $encoders = [new JsonEncoder()];
+        $serializer = new Serializer($normalizer, $encoders);
+        $serializer->deserialize(
+            $this->serializedSimulation,
+            Simulation::class,
+            self::SERIALIZE_FORMAT,
+            [
+                AbstractNormalizer::OBJECT_TO_POPULATE      => $simulation,
+            ]
+        );
+
+        return $simulation;
+    }
+
+    private function createRawSimulation()
+    {
+        $simulation = new Simulation();
+        $simulation
+            ->setParameters('')
+            ->setAlias('')
+            ->setCategory('')
+            ->setToken('')
+            ->setActive(true)
+            ->setResponseDelay(0)
+            ->setTtl(15)
+            ->setCreated(new DateTime());
+
+        return $simulation;
+    }
+}
---
+++ tests/SimulationImporterTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Tests;
+
+use App\Entity\Simulation;
+use PHPUnit\Framework\TestCase;
+use App\Service\SimulationImporter;
+
+class SimulationImporterTest extends TestCase
+{
+    public function testImport()
+    {
+        $simulationJson = '{"category":"products","token":"search","httpVerb":"POST","requestBodyContent":"Quis quibusdam voluptatibus quaerat omnis non. Tempora cupiditate modi velit ea possimus illo a. Explicabo et repellat et hic quaerat cum.","responseBodyContent":"Response","responseContentType":"text","responseCode":200,"responseDelay":0,"alias":"Cute Alias"}';
+
+        $simulationImporter = new SimulationImporter($simulationJson);
+
+        $simulation = $simulationImporter->loadSimulation();
+
+        $this->assertInstanceOf(Simulation::class, $simulation);
+    }
+}
